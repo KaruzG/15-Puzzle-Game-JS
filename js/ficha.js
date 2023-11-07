0c5f3666e9e5db7676fbc1c916bd38c1d3fc4adf
Karuz <josemiguelcpcs@gmail.com>
2023-11-07 10:37:32 +0100

    Generación de tabla con las fichas en orden random

--- js/ficha.js
+++ js/ficha.js
@@ -4,15 +4,18 @@ class ficha {
      * 
      * @param {Number} valor Valor de la ficha
      * @param {String} img Src de la imagen
+     * @param {Number} posX Posición en el eje X (-2 a 2)
+     * @param {Number} posY Posición en el eje Y (-2 a 2)
      */
 
-    constructor(valor) {
+    constructor(valor, posX, posY) {
         this.valor = valor;
-        this.img = "./img/"+(valor)+".gif";
+        this.img = "./img/"+(valor+1)+".gif";
     }
 
     // Methods
     displayFicha() {
-        return "<img id=\"ficha"+ valor +"\" src=\""+ img +"\"</img>";
+        console.log("Display!");
+        return "<img id=\"ficha"+ (this.valor + 1) +"\" src=\""+ this.img +"\"</img>";
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 var body = document.querySelector("body");
 
-// Crear tabla 4x4
-function crearTabla4x4() {
 
-    // Genera una tabla con sus tr y td y se inicializa un objeto ficha cada td menos en 1 td random.
+function crearTabla4x4(fichas) {
+    // Seleccionar aleatoriamente que celda queda vacía:
+    /*
+        Aunque da un valor entre -2 a 1 (tipo coordenada) luego se va a usar
+        de 1 a 4 en la creación de la tabla.
+    */
+    let celdaVaciaX = (Math.floor(Math.random() * 4 - 2)) ;
+    let celdaVaciaY = (Math.floor(Math.random() * 4 - 2));
+    console.log("· Hueco: x:" + celdaVaciaX + "  y:"+celdaVaciaY);
+
+    // Aleatorizar array (Algoritmo Durstenfeld shuffle)
+    for (let i = fichas.length - 1; i > 0; i--) {
+        const i2 = Math.floor(Math.random() * (i + 1));
+        [fichas[i], fichas[i2]] = [fichas[i2], fichas[i]];
+    }
+    console.log("· Fichas mezcladas");
+
+    // Genera una tabla con sus tr y td y se inicializa un objeto ficha cada td menos
+    // en el lugar aleatorio anteriormente calculado.
     let tabla = "<table>";
+
+    let fichasCreadas = 0;
     for(let columnasCreadas = 1; columnasCreadas <= 4; columnasCreadas++) {
         tabla += "<tr>"
         for( let filasCreadas = 1; filasCreadas <= 4; filasCreadas++) {
-            // Inicializar ficha:
-            let ficha
-
-
             // Contenido de la celda:
-            tabla += "<td>" + "a" + "</td>";
+            console.log("FilasCreadas:" + filasCreadas + " " + (celdaVaciaX+3));
+            console.log("FilasCreadas:" + columnasCreadas + " " + (celdaVaciaY+3));
+            console.log(fichasCreadas);
+
+            if(columnasCreadas == (celdaVaciaY+3) && filasCreadas == (celdaVaciaX + 3)) {
+                console.log("NO MUESTRO");
+                tabla += "<td></td>"
+            } else {
+                tabla += "<td>" + fichas[fichasCreadas].displayFicha() + "</td>";
+                fichasCreadas++;
+            }
         }
         tabla += "</tr>"
     }
@@ -23,6 +47,16 @@ function crearTabla4x4() {
     body.innerHTML += tabla;
 }
 
-// Generar ficha
 
-crearTabla4x4();
+function generarFichas() {
+    // Las fichas serán almacenadas en un array
+    let fichas = [];
+    for (let i = 0; i < 15; i++) {
+        fichas.push(new ficha(i));
+    }
+    console.log("· Fichas generadas");
+
+    return fichas;
+}
+
+crearTabla4x4(generarFichas());
